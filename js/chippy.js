f77ec06c6fd3695356d770cd02895df786a3f30b
Brant Olsen <brantolsen@gmail.com>
2012-04-30 06:54:05 -0400

    Add the leaderboard and a button to show the leaderboard.

--- js/chippy.js
+++ js/chippy.js
@@ -1,16 +1,49 @@
-// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
-function AppViewModel() {
-    this.firstName = ko.observable("Bert");
-    this.lastName = ko.observable("Bertington");
-    this.fullName = ko.computed(function() {
-      return this.firstName() + " " + this.lastName();    
-    }, this);
-    
-    this.capitalizeLastName = function() {
-        var currentVal = this.lastName();        // Read the current value
-        this.lastName(currentVal.toUpperCase()); // Write back a modified value
-    };
+// Class to represent a row in the seat reservations grid
+function Score(name, score, date) {
+    var self = this;
+    self.name = name;
+    self.score = score;
+    self.date = date;
 }
 
-// Activates knockout.js
-ko.applyBindings(new AppViewModel());
+// Overall viewmodel for this screen, along with initial state
+function LeaderboardViewModel() {
+    var self = this;
+    self.scores = ko.observableArray();
+    self.leaderboard = ko.observable(false);
+
+    // Operations
+    self.addScore = function(name, score) {
+        self.scores.push(new Score(name, score, new Date()));
+        self.save();
+    }
+
+    self.load = function() {
+        if (localStorage.getItem("chippy_leaderboard"))
+        {
+            var existingLeaderboard = JSON.parse(localStorage.getItem("chippy_leaderboard"));
+            for (var index = 0; index < existingLeaderboard.length; ++index)
+            {
+                self.scores.push(existingLeaderboard[index]);
+            }
+        }
+    }
+
+    self.save = function() {
+        var leaderBoardToSave = new Array();
+        for(var index = 0; index < self.scores().length; ++index)
+        {
+            leaderBoardToSave.push(self.scores()[index]);
+        }
+        localStorage.setItem("chippy_leaderboard", JSON.stringify(leaderBoardToSave));
+    }
+
+    self.load();
+}
+
+LeaderboardViewModel.prototype.showLeaderboard = function()
+{
+  this.leaderboard(true);
+}
+
+ko.applyBindings(new LeaderboardViewModel());
